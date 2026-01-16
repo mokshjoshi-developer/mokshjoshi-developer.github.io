2f42344e0677586118ad8bb21752eeec3f97a2ac
Moksh Joshi <mokshjoshi3910@gmail.com>
2026-01-16 18:16:34 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+document.getElementById("qrcode-view").addEventListener("click", () => {
+    window.location.href = 'https://mokshjoshi-developer.github.io/qrcodegenerator/'
+});
+
+document.getElementById("qrcode-github").addEventListener("click", () => {
+    window.location.href = 'https://github.com/mokshjoshi-developer/qrcodegenerator/'
+})
+
+document.getElementById("calculator-view").addEventListener("click", () => {
+    window.location.href = 'https://mokshjoshi-developer.github.io/calculator/'
+})
+
+document.getElementById("calculator-github").addEventListener("click", () => {
+    window.location.href = 'https://github.com/mokshjoshi-developer/calculator/'
+})
+
+document.getElementById("document-view").addEventListener("click", () => {
+    window.location.href = 'https://mokshjoshi-developer.github.io/ConvertPro/'
+})
+
+document.getElementById("document-github").addEventListener("click", () => {
+    window.location.href = 'https://github.com/mokshjoshi-developer/ConvertPro/'
+})
