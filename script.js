de9aee908cde3e0909afbafdbabc68597ddb2f90
Moksh Joshi <mokshjoshi3910@gmail.com>
2026-01-18 11:52:00 +0530

    Update URLs in script.js to new GitHub accounts

--- script.js
+++ script.js
@@ -1,23 +1,24 @@
 document.getElementById("qrcode-view").addEventListener("click", () => {
-    window.location.href = 'https://mokshjoshi-developer.github.io/qrcodegenerator/'
+    window.location.href = 'https://moxjoshi.github.io/qrcodegenerator/'
 });
 
 document.getElementById("qrcode-github").addEventListener("click", () => {
-    window.location.href = 'https://github.com/mokshjoshi-developer/qrcodegenerator/'
+    window.location.href = 'https://github.com/moxjoshi/qrcodegenerator/'
 })
 
 document.getElementById("calculator-view").addEventListener("click", () => {
-    window.location.href = 'https://mokshjoshi-developer.github.io/calculator/'
+    window.location.href = 'https://moxjoshi.github.io/calculator/'
 })
 
 document.getElementById("calculator-github").addEventListener("click", () => {
-    window.location.href = 'https://github.com/mokshjoshi-developer/calculator/'
+    window.location.href = 'https://github.com/moxjoshi/calculator/'
 })
 
 document.getElementById("document-view").addEventListener("click", () => {
-    window.location.href = 'https://mokshjoshi-developer.github.io/ConvertPro/'
+    window.location.href = 'https://moxjoshi.github.io/ConvertPro/'
 })
 
 document.getElementById("document-github").addEventListener("click", () => {
-    window.location.href = 'https://github.com/mokshjoshi-developer/ConvertPro/'
+    window.location.href = 'https://github.com/moxjoshi/ConvertPro/'
+
 })
